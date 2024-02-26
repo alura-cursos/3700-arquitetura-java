203169a12e36862c9f2d048fe70764e14b9c179a
Rodrigo Caneppele <r.caneppele@gmail.com>
2024-02-26 11:25:21 -0300

    4.2 - Configurando cache na aplicacao

--- src/main/java/br/com/alura/codechella/CodechellaApplication.java
+++ src/main/java/br/com/alura/codechella/CodechellaApplication.java
@@ -2,10 +2,12 @@ package br.com.alura.codechella;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableCaching
 public class CodechellaApplication {
 
 	public static void main(String[] args) {
